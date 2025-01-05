8717791c1f73502db10809cefcce3997b3c367cf
Sumit Dutta <sumitdutta2810@gmail.com>
2025-01-06 02:10:13 +0530

    Update Feed.jsx

--- src/components/feed/Feed.jsx
+++ src/components/feed/Feed.jsx
@@ -13,7 +13,7 @@ function Feed({username}) {
 
   useEffect(()=>{
     const fetchPosts = async()=>{
-      const res = username? await axiosInstance.get("/api/posts/profile/"+username) :await axios.get("/posts/timeline/all/"+user._id)
+      const res = username? await axiosInstance.get("/api/posts/profile/"+username) :await axiosInstance.get("/api/posts/timeline/all/"+user._id)
       setPosts(res.data.sort((p1,p2)=>{
         return new Date(p2.createdAt) - new Date(p1.createdAt)
       }))
